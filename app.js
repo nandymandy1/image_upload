5b4a052c6074675a32a3332a2731ab857cf32e49
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-09-18 23:30:56 +0530

    Image Upload service

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = 5000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`)
+});
