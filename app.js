6a7e860027b09ea8d196696c08ea0d08cfedf6e6
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-09-18 23:49:01 +0530

    DEV Env port and default image uploaded

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT;
+let devEnv = false;
+
+const PORT = devEnv ? 5000 : process.env.PORT;
 
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/butterfly.jpg'));
 });
 
 app.listen(PORT, () => {
